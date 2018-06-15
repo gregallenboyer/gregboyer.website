9e4bfbbbdb07481b1842c2b7f4656f98e4e4d6bf
gregallenboyer <40300957+gregallenboyer@users.noreply.github.com>
2018-06-15 11:37:53 -0500

    Add files via upload

--- js/resume.js
+++ js/resume.js
@@ -1,1 +1,64 @@
-
+﻿$(function () {
+    $('#collapseLLT').on('hide.bs.collapse', function () {
+        $('#buttonLLT').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    })
+    $('#collapseLLT').on('show.bs.collapse', function () {
+        $('#buttonLLT').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    })
+})
+$(function () {
+    $('#collapseExp').on('hide.bs.collapse', function () {
+        $('#buttonExp').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    })
+    $('#collapseExp').on('show.bs.collapse', function () {
+        $('#buttonExp').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    })
+})
+$(function () {
+    $('#collapsePalmerT').on('hide.bs.collapse', function () {
+        $('#buttonPalmerT').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    })
+    $('#collapsePalmerT').on('show.bs.collapse', function () {
+        $('#buttonPalmerT').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    })
+})
+$(function () {
+    $('#collapseAEGONT').on('hide.bs.collapse', function () {
+        $('#buttonAEGONT').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    })
+    $('#collapseAEGONT').on('show.bs.collapse', function () {
+        $('#buttonAEGONT').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    })
+})
+$(function () {
+    $('#collapseStockpointT').on('hide.bs.collapse', function () {
+        $('#buttonStockpointT').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    })
+    $('#collapseStockpointT').on('show.bs.collapse', function () {
+        $('#buttonStockpointT').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    })
+})
+$(function () {
+    $('#collapseMcGladreyT').on('hide.bs.collapse', function () {
+        $('#buttonMcGladreyT').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    })
+    $('#collapseMcGladreyT').on('show.bs.collapse', function () {
+        $('#buttonMcGladreyT').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    })
+})
+$(function () {
+    $('#collapseNavyT').on('hide.bs.collapse', function () {
+        $('#buttonNavyT').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    })
+    $('#collapseNavyT').on('show.bs.collapse', function () {
+        $('#buttonNavyT').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    })
+})
+$(function () {
+    $('#collapseEdu').on('hide.bs.collapse', function () {
+        $('#buttonEdu').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    })
+    $('#collapseEdu').on('show.bs.collapse', function () {
+        $('#buttonEdu').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    })
+})
